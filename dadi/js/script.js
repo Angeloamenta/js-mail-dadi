9bc7f19d1c24316139756c8c1e2084cece6eed54
Angeloamenta <angeloamenta7@gmail.com>
2021-11-24 16:40:40 +0100

    da 1 a 10

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -5,19 +5,19 @@
 const button = document.getElementById("button");
 
 button.addEventListener("click", function(){
-    let randNumberOne = Math.floor(Math.random() * 10);
+    let randNumberOne = Math.floor(Math.random() * 6)+1;
     console.log (randNumberOne)
 
-    let randNumberTwo = Math.floor(Math.random() * 10);
+    let randNumberTwo = Math.floor(Math.random() * 6)+1;
     console.log (randNumberTwo)
 
     const winner = document.getElementById("risultato");
 
     if (randNumberOne > randNumberTwo) {
-        winner.append(randNumberOne, randNumberTwo, " player win");
+        winner.append(randNumberOne," - ", randNumberTwo," - ", " Player win");
 
     }else if (randNumberOne < randNumberTwo) {
-        winner.append(randNumberOne, randNumberTwo, " pc win");
+        winner.append(randNumberOne," - ", randNumberTwo," - "," Pc win");
     }else {
         winner.append("Pair")
     }
